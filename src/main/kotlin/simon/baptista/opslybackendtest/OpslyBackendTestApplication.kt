b2112c286a0e38962bcdd82e6cc7a931fb8d0cc4
Simon Baptista <simonbaptista@hotmail.com>
2020-06-28 16:55:25 +0100

    initialise Spring Boot project. First Commit.

---
+++ src/main/kotlin/simon/baptista/opslybackendtest/OpslyBackendTestApplication.kt
@@ -0,0 +1,11 @@
+package simon.baptista.opslybackendtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpslyBackendTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<OpslyBackendTestApplication>(*args)
+}
