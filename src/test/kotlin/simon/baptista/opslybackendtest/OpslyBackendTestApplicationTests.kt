e9d88a43be0c987954f234bf81ac1f4cc3f79435
Simon Baptista <simonbaptista@hotmail.com>
2020-06-28 18:18:41 +0100

    Add first integration test for status is OK

--- src/test/kotlin/simon/baptista/opslybackendtest/OpslyBackendTestApplicationTests.kt
+++ src/test/kotlin/simon/baptista/opslybackendtest/OpslyBackendTestApplicationTests.kt
@@ -1,13 +1,24 @@
 package simon.baptista.opslybackendtest
 
 import org.junit.jupiter.api.Test
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class OpslyBackendTestApplicationTests {
 
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
     @Test
-    fun contextLoads() {
+    fun root_returns_status_is_OK() {
+        mockMvc.perform(get("/")).andExpect(status().isOk)
     }
 
 }
